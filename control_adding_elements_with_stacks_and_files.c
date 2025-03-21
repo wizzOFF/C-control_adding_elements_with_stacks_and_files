93bff153473f7ae8744e27f6808497400ba55a55
WizzOFF <maher_off@proton.me>
2025-03-21 03:22:09 +0100

    control_adding_elements_with_stacks_and_files.c: build a stack structure > unstacking an element

--- control_adding_elements_with_stacks_and_files.c
+++ control_adding_elements_with_stacks_and_files.c
@@ -24,3 +24,23 @@ void empiler(Pile *pile, int nvNombre)
     nouveau->suivant = pile->premier;
     pile->premier = nouveau;
 }
+
+int depiler(Pile *pile)
+{
+    if (pile == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    int nombreDepile = 0;
+    Element *elementDepile = pile->premier;
+
+    if (pile != NULL && pile->premier != NULL)
+    {
+        nombreDepile = elementDepile->nombre;
+        pile->premier = elementDepile->suivant;
+        free(elementDepile);
+    }
+
+    return nombreDepile;
+}
