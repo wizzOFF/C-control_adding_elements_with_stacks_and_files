32bebc36d70dc14a5d120faa61f68eb36fff868e
WizzOFF <maher_off@proton.me>
2025-03-23 23:26:46 +0100

    control_adding_elements_with_stacks_and_files.c: build a file structure > add an element to the file

--- control_adding_elements_with_stacks_and_files.c
+++ control_adding_elements_with_stacks_and_files.c
@@ -98,3 +98,30 @@ struct File
 {
     Element *premier;
 };
+
+void enfiler(File *file, int nvNombre)
+{
+    Element *nouveau = malloc(sizeof(*nouveau));
+    if (file == NULL || nouveau == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    nouveau->nombre = nvNombre;
+    nouveau->suivant = NULL;
+
+    if (file->premier != NULL) /* La file n'est pas vide */
+    {
+        /* On se positionne à la fin de la file */
+        Element *elementActuel = file->premier;
+        while (elementActuel->suivant != NULL)
+        {
+            elementActuel = elementActuel->suivant;
+        }
+        elementActuel->suivant = nouveau;
+    }
+    else /* La file est vide, notre élément est le premier */
+    {
+        file->premier = nouveau;
+    }
+}
